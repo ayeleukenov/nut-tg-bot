fa8fc1ee9d05f20495f3965e35469b6ae1d62999
Елеукенов Ахмет <912092@gmail.com>
2025-04-08 20:12:10 +0500

    configured admin.py in nuts_tg_app

--- nuts_tg_bot_src/nuts_tg_app/admin.py
+++ nuts_tg_bot_src/nuts_tg_app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from nuts_tg_app.models import Category, CategoryItem
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+    fields = ["name"]
+    
+
+class CategoryItemAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "category"]
+    fields = ["name", "category"]
+    
+    
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(CategoryItem, CategoryItemAdmin)
+
