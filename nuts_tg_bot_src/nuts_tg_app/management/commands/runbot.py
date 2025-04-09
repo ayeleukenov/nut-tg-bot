c5405776e6dfa7e6cebd123c0dd6243c6cb2eb28
Елеукенов Ахмет <912092@gmail.com>
2025-04-09 15:05:14 +0500

    amended runbot code

--- nuts_tg_bot_src/nuts_tg_app/management/commands/runbot.py
+++ nuts_tg_bot_src/nuts_tg_app/management/commands/runbot.py
@@ -1,19 +1,24 @@
+import asyncio
+
 from django.core.management.base import BaseCommand
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import Bot
 from django.conf import settings
 
 
 class Command(BaseCommand):
-    help = 'Бот работает вместе с параллельным запуском django сессии'
+    help = 'Устанавливает webhook на телеграм бот'
     
     def handle(self, *args, **options):
-        async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-            await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+        bot = Bot(token=settings.TELEGRAM_TOKEN)
         
-        app = ApplicationBuilder().token(settings.TELEGRAM_TOKEN).build()
-        app.add_handler(CommandHandler("start", start))
+        async def set_webhook_async():
+            webhook_url = f'https://{settings.NGROK_DOMAIN}/webhook/'
+            success = await bot.set_webhook(webhook_url)
+            
+            if success:
+                self.stdout.write(self.style.SUCCESS(f'Successfully set webhook to {webhook_url}'))
+            else:
+                self.stdout.write(self.style.ERROR('Failed to set webhook'))
         
-        self.stdout.write(self.style.SUCCESS('Starting Telegram bot'))
-        app.run_polling()
+        asyncio.run(set_webhook_async())
         
